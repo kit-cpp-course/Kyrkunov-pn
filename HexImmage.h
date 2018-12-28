897d48ab15d8c07dfd0cae4e731b5d914adcc4af
Kyrkunov <37331950+pashakyrkunov@users.noreply.github.com>
2018-12-28 04:06:34 +0300

    Update HexImmage.h

--- HexImmage.h
+++ HexImmage.h
@@ -5,20 +5,20 @@ class HexImmage
 private:
 	std::map<std::string, int> hexColorCount;
 	/*
-
-	* ÐÐ°Ð·Ð²Ð°Ð½Ð¸Ðµ Ð¸Ð·Ð¾Ð±ÑÐ°Ð¶ÐµÐ½Ð¸Ñ
+	* Название изображения
 	*/
 	std::string im_name;
 	/*
-	* ÐÑÑÑ Ðº Ð¸Ð·Ð¾Ð±ÑÐ°Ð¶ÐµÐ½Ð¸Ñ
+	* Путь к изображению
 	*/
 	std::string im_path;
 	/*
-	* ÐÐ¾Ð¼Ð¸Ð½Ð¸ÑÑÑÑÐ¸Ð¹ ÑÐ²ÐµÑ Ð² Ð¸Ð·Ð¾Ð±ÑÐ°Ð¶ÐµÐ½Ð¸Ð¸
+	* Доминирующий цвет в изображении
 	*/
 	std::string dominateColor;
 	
 public:
 	HexImmage();
+	
 };
 
